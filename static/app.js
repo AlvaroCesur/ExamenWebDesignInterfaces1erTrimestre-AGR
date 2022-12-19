6d6e6e67f48bda2cf9a3a0e259e7f57b519288a9
AlvaroCesur <93986162+AlvaroCesur@users.noreply.github.com>
2022-12-19 12:02:24 +0100

    Examen

--- static/app.js
+++ static/app.js
@@ -1,3 +1,22 @@
-// nombre_del_alumno
+// Alvaro Gonzalez Rando
 //
 // Completar JS aqui
+
+var tabla = document.querySelector("#contenidoTabla")
+var modal = document.querySelector("#modal");
+var add = document.querySelector(".verde");
+var back = document.querySelector(".gris");
+
+add.onclick = function() {
+
+    tabla.style.display = "none";
+    modal.style.display = "flex";
+
+}
+
+back.onclick = function() {
+
+    modal.style.display = "none";
+    tabla.style.display = "flex";
+
+}
